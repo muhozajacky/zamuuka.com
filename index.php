86b0b02ece6d1eb57db61cb6d553b61bf2d27ce1
muhozajacky <137993979+muhozajacky@users.noreply.github.com>
2023-06-28 10:58:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>home</title>
+	<link rel="stylesheet" type="style/css" href="style.css">
+</head>
+<body>
+<ul>
+	<li>HOME</li>
+	<li>ABOUT US</li>
+	<li>SERVICE</li>
+	<li>CONTACT US</li>
+</ul>
+<section class="me">
+	<div class="you">
+		<p>WELCAME TO MY WEBSITE</p>
+		</div> </section>
+		<footer >
+			<div class ="footer">
+			<h3 style="color: white; font-size:80px;"> CONTACT </h3>
+			<p> tel:078373456</p>
+			<p> bugesera</p>
+			<div>
+
+</footer>
+
+</body>
+</html>
